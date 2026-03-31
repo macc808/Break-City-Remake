7c4dea9bd349ea2a0ba3d58a69e20a589c03f8d4
macc808 <pismenukmaksim@gmail.com>
2026-03-31 22:13:26 +0300

    nothing changes

--- config.py
+++ config.py
@@ -8,9 +8,3 @@ TILE_IMAGES = {
     1: "assets/images/floor.jpg",
     2: "assets/images/wall.jpg",
 }
-
-# Старий словник для сумісності
-block_img = {
-    1: "assets/images/wall.png",
-    2: "assets/images/title1.png",
-}
