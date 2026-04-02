523c3ffa20e41e1a1baffe091efb23df25dcc9f1
macc808 <pismenukmaksim@gmail.com>
2026-04-02 12:14:15 +0300

    fixes

--- logger.py
+++ logger.py
@@ -1,6 +1,6 @@
 class Logger:
     def __init__(self):
-        self.filename = "game_log.log"
+        self.filename = "game_log.txt"
 
     def log_message(self, func, res):
         with open(self.filename, 'a') as f:
